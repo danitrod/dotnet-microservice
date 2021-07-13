1b38b99e2424fc5bd7a93136c9693bd95f24ba9c
danitrod <danitrod@outlook.com>
2021-07-13 02:37:17 -0300

    add mongodb integration

--- src/Play.Catalog.Service/Dtos.cs
+++ src/Play.Catalog.Service/Dtos.cs
@@ -1,9 +1,11 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
+// DTO - data transfer object
+// used for agreeing the data used with both parts communicating through the API
 namespace Play.Catalog.Service.Dto
 {
-  public record ItemDto(Guid Id, [Required] string Name, string Description, [Range(0, 1000)] decimal Price, DateTimeOffset CreatedDate);
+  public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
 
   public record CreateItemDto([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
 
